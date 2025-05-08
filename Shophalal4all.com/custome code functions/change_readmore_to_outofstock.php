8e49ac5f1904a9c80595e05a7f83dea9e86208a9
Kajetan <130190258+KayoSKI05@users.noreply.github.com>
2025-05-09 00:19:44 +0200

    Change file name

--- Shophalal4all.com/custome code functions/change_readmore_to_outofstock.php
+++ Shophalal4all.com/custome code functions/change_readmore_to_outofstock.php
@@ -3,7 +3,7 @@
 add_filter( 'woocommerce_product_add_to_cart_text', 'zmien_tekst_przycisku_woocommerce' ); 
 function zmien_tekst_przycisku_woocommerce( $tekst ) {
     if ( 'Read more' === $tekst ) {
-        return 'Variations';
+        return 'Out of stock';
     }
     return $tekst;
 }
